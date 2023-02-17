0b70ee1456408dfdbc51705a4742788b0710b5d5
yora132 <yarasalah132@gmail.com>
2023-02-17 12:15:23 -0800

    numberzz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints numbera from 1 to 9
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+
+}
