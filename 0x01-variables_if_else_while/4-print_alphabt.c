94a7fd46bee95b41d0777220b6b073866c1a62c2
yora132 <yarasalah132@gmail.com>
2023-02-17 10:04:46 -0800

    alpha without e q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the alphabet excpet e q
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c == 'q' || c == 'e')
+		{
+			continue;
+		}
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+
+}
