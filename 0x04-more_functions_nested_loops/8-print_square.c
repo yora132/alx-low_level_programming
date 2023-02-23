5d67dff026a3284bd0269a7745c323dfbea3cb95
yora132 <yarasalah132@gmail.com>
2023-02-23 14:03:28 -0800

    done

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,12 +14,15 @@ void print_square(int size)
 	for (h = 0; h < size; h++)
 	{
 		for (w = 0; w < size; w++)
-		{
+
 			_putchar('#');
+
+
+			if (w == size - 1)
+				continue;
 			_putchar('\n');
-		}
+
 	}
 	}
-	else
 		_putchar('\n');
 }
