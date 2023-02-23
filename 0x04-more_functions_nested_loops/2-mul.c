7b5f20d191a25d2cba2a795082231eaac29d0617
yora132 <yarasalah132@gmail.com>
2023-02-23 08:27:45 -0800

    multipluy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - descrption multipluy 2 int
+ * @a: first int
+ * @b: second int
+ * return: addition of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
