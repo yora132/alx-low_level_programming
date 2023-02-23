ed99a5ca001e69fb5120b4eb9aff04ce6f376532
yora132 <yarasalah132@gmail.com>
2023-02-23 11:49:11 -0800

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - descrption print line
+ * @n: the number of _ charctes
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line <= n; line++)
+		{
+		_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
