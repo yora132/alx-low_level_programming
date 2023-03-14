c6df9ad2156be95c67c1a7347b1cfa10859c1f11
yora132 <yarasalah132@gmail.com>
2023-03-14 10:32:07 -0700

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,7 +37,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
-	for (j = 0; j < len2; j++)
+	for (j = 0; j < len2; j++; i++)
 		result[i] = s2[j];
 	result[len] = '\0';
 
