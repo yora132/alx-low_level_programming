206513c12bcf2cabf238d5408c62ae8254af6377
yora132 <yarasalah132@gmail.com>
2023-03-22 15:37:49 -0700

    done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,6 +26,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit (99);
 	}
-	printf("%d\n,oprt(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n",oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
