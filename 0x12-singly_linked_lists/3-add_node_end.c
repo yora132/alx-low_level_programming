07443a379ebdbf82d87d22298eb8a1b91a61016a
yora132 <yarasalah132@gmail.com>
2023-03-30 15:34:36 -0700

    done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -39,10 +39,6 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 
-	if (new->str == NULL)
-
-		free(new);
-	return (NULL);
 	new->len = _strlen(new->str);
 	new->next = NULL;
 	if (*head == NULL)
@@ -54,7 +50,5 @@ list_t *add_node_end(list_t **head, const char *str)
 	while (tmp->next)
 		tmp = tmp->next;
 	tmp->next = new;
-	return (new);
+	return (*head);
 }
-
-
