0875a46e46b8efb51871cb923a69c920def37b13
yora132 <yarasalah132@gmail.com>
2023-03-30 16:35:56 -0700

    done

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+	global	main
+
+	section	.text
+main:	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, msg
+	mov	rdx, 13
+	syscall
+	mov	rax, 60
+	xor	rdi, rdi
+	syscall
+
+	section	.data
+msg:	db	"Hello, Holberton", 10
