6abd23471f9de3bef9537d8cce0efc5c48332a63
yora132 <yarasalah132@gmail.com>
2023-04-04 15:46:11 -0700

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,11 +17,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	if (idx != 0)
+	{
 		ptr = *head;
 	for (i = 0; i < idx - 1 && ptr != NULL; i++)
 	{
 		ptr = ptr->next;
-
+	}
 		if (ptr == NULL)
 			return (NULL);
 	}
