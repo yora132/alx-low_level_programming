e99cbdeed997050293620f563bd8a9d3ed5f1e62
yora132 <yarasalah132@gmail.com>
2023-04-04 16:17:11 -0700

    task10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete node at index
+ * @head:double pointer to list
+ * @index:index of lisrt element
+ *
+ *Return: succsse 1 and -1 in fail
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *ptr, *ptr2;
+	unsigned int i;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		ptr2 = (*head)->next;
+		free(*head);
+		*head = ptr2;
+	}
+
+	ptr = *head;
+
+	for (i = 0; i < index - 1; i++)
+	{
+		if (ptr->next == NULL)
+		return (-1);
+
+		ptr = ptr->next;
+	}
+
+	ptr2 = ptr->next;
+	ptr->next = ptr2->next;
+	free(ptr2);
+	return (1);
+}
+
