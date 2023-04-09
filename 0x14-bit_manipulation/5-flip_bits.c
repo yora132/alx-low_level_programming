17f970154c0b75e8133cd58a8d160aec500f678e
yora132 <yarasalah132@gmail.com>
2023-04-09 08:51:05 -0700

    done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,8 +4,20 @@
  * flip_bits - filp the bits
  * @n:first number
  * @m:second number
- * Return:the number of bits 
+ * Return:the number of bits
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int count = 0;
+	unsigned long int diff;
+
+	diff = n ^ m;
+
+	while (diff != 0)
+	{
+		count += diff & 1;
+		dif >>= 1;
+	}
+	return (count);
+}
