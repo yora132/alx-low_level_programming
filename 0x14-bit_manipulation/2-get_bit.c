51c003182c90da66581e9a8a1abf336d82105c22
yora132 <yarasalah132@gmail.com>
2023-04-09 08:17:58 -0700

    done

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,8 +4,12 @@
  * get_bit - print the bit in a special index
  * @n:the number
  * @index:the place of the bit
- * Return: the value of the bit at index 
+ * Return: the value of the bit at index
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	else
+		return ((n >> index) & 1);
