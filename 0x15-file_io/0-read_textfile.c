beff583c981257f54402fd79a715948033ae425b
yora132 <yarasalah132@gmail.com>
2023-04-10 12:51:54 -0700

    done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - read the file
+ * @filename:the name of file
+ * @letters:size that will bw read
+ * Return:number of read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t num_read, num_write;
+	char *buffer;
+	int fp;
+
+	if (!filename)
+		return (0);
+
+	fp = open(filename, O_RDONLY);
+
+	if (!fp)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+
+	if (!buffer)
+	{
+		return (0) ;
+	}
+
+	num_read = read(fp, buffer, letters);
+	num_write = write(STDOUT_FILENO, buffer, num_read);
+
+	close(fp);
+
+	free(buffer);
+
+	return (num_write);
+}
