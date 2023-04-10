b95557e8c7de164c73918a4c74607d15f7feb278
yora132 <yarasalah132@gmail.com>
2023-04-10 13:36:42 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
  * create_file - function to create file
  * @filename:pointer to file name
  * @text_content:contet to be written
- * Return:1 on success, -1 on failure 
+ * Return:1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
@@ -12,13 +12,14 @@ int create_file(const char *filename, char *text_content)
 	size_t len;
 	int fp;
 	ssize_t ret;
+
 	if (filename == NULL)
 		return (-1);
 
-	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSER);
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fp == -1)
-		return -1;
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -27,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 		if (ret == -1)
 		{
-			close (fp);
+			close(fp);
 			return (-1);
 		}
 	}
