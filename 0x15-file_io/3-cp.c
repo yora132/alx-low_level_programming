bd3a2c1d0baa242d8edf37cb52e27531d6707ef5
yora132 <yarasalah132@gmail.com>
2023-04-10 17:31:38 -0700

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,65 +1,47 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
- *
+ * main - cope from file to anther
+ * @argc:numbers of argmmunt
+ * @argv:array of argmmunt
+ * Return:number
  */
-
 int main(int argc, char *argv[])
-{
-	int fd_from, num_read;
-	int fd_to, num_write;
+{	int fd_from, num_read, fd_to, num_write;
 	char buf[BUF_SIZE];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
-
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 	S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
 	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	while ((num_read = read(fd_from, buf, BUF_SIZE)) > 0)
-	{
-		if ((num_write = write(fd_to, buf, num_read)) != num_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",argv[2]);
+	{	num_write = write(fd_to, buf, num_read);
+		if (num_write != num_read)
+		{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-
 	if (num_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-	
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 	return (0);
