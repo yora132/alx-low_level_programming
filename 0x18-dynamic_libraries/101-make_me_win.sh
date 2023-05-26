798b44c9b69870a5b2f5c2614279a12f4fdd9fa6
yora132 <yarasalah132@gmail.com>
2023-05-26 10:00:52 -0700

    done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/yora132/alx-low_level_programming/tree/main/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp/ https://github.com/yora132/alx-low_level_programming/tree/main/0x18-dynamic_libraries/ywin.so
+export LD_PRELOAD=/tmp/ywin.so
